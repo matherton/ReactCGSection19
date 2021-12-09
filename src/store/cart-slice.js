3da57415fdc586859ade69f6d393b6132b0fb1e3
Mark Atherton <mark@insightinternet.co.uk>
2021-12-09 14:25:18 +0000

    pushed a new item onto items array

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -12,7 +12,13 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       if (!existingItem) {
-        state.items.push();
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
       }
     },
     removeItemFromCart() {},
