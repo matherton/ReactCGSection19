8a2de7b641cd6d4e4437b4fb93fa8669f4b4a84d
Mark Atherton <mark@insightinternet.co.uk>
2021-12-09 13:45:37 +0000

    imported methods from react-redux and ui slice to create toggleCartHandler

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const disPatch = useDispatch();
+  const toggleCartHandler = (state) => {
+    disPatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
