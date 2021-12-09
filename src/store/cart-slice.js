4a92e34f8bf2e147ea3cdd70e14815fbf9c05949
Mark Atherton <mark@insightinternet.co.uk>
2021-12-09 14:02:20 +0000

    started creating cartSlice using createSlice to define inial key pair values

---
+++ src/store/cart-slice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    totalAmount: 0,
+  },
+  reducers: {},
+});
