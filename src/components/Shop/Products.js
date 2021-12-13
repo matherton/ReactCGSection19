cbfafbeaff799664987ce33ffb70bcdd4e0c44f0
Mark Atherton <mark@insightinternet.co.uk>
2021-12-13 11:26:03 +0000

    added a DUMMY_PRODUCTS array and added the 1st object into the <ProductItem /> component

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,5 +1,20 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "My First Book",
+    price: 6,
+    description: "the 1st book I ever wrote",
+  },
+  {
+    id: "p2",
+    title: "My Second Book",
+    price: 4,
+    description: "the 2nd book I ever wrote",
+  },
+];
 
 const Products = (props) => {
   return (
@@ -7,9 +22,9 @@ const Products = (props) => {
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+          title={DUMMY_PRODUCTS[0].title}
+          price={DUMMY_PRODUCTS[0].price}
+          description={DUMMY_PRODUCTS[0].description}
         />
       </ul>
     </section>
