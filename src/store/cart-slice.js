f637c4fe19dd9f0d1d6a595fd9461366b0f55323
Mark Atherton <mark@insightinternet.co.uk>
2021-12-13 10:57:15 +0000

    added IF logic to filter out the single found item in initial IF check and leave all the other items that don't metch the id in the array

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -28,6 +28,7 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantiy--;
       }
