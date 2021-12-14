e29c2761dc6670c5887226c7a7d94e813b7a8fb4
Mark Atherton <mark@insightinternet.co.uk>
2021-12-14 10:49:18 +0000

    replaced hardcoded 1 with dynamic cartQuantity

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -13,7 +13,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
