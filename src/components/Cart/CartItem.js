2ca7bd67260f1c1d011e1643616d05e6fd2ea643
Mark Atherton <mark@insightinternet.co.uk>
2021-12-14 11:31:14 +0000

    added id prop to items object prop in Cart.js and used as a payload reference in decrementItemHandler's dispatch method

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,11 +1,17 @@
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+
 import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch();
+
+  const decrementItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
 
   const incrementItemHandler = () => {};
-
-  const decrementItemHandler = () => {};
 
   return (
     <li className={classes.item}>
