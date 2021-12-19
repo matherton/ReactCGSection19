c090f2ce9dd1ed2717880428204c2e1b6f30efef
Mark Atherton <mark@insightinternet.co.uk>
2021-12-19 12:22:00 +0000

    removed showNotification and fetch handling code

--- src/App.js
+++ src/App.js
@@ -19,25 +19,6 @@ function App() {
     const sendCartData = async () => {
       dispatch(
         uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending Cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-4b88b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed!!!");
-      }
-
-      dispatch(
-        uiActions.showNotification({
           status: "success",
           title: "Success!",
           message: "Sent Cart data successfully",
